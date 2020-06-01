58434acc611f01b5080e60ffcf7fc8227d1deb8e
Faiq Javed <fj.261996@gmail.com>
2020-06-02 04:06:47 +0500

    First block created.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,17 @@
 module.exports = {
   purge: [],
   theme: {
+    borderRadius: {
+      'none': '0',
+      'sm': '0.125rem',
+      default: '0.25rem',
+      default: '4px',
+      'md': '0.375rem',
+      'lg': '0.5rem',
+      'full': '9999px',
+      'large': '12px',
+      'xl': '0.75rem'
+    },
     extend: {
       colors: {
         'almost-black': '#3d3d3d',
